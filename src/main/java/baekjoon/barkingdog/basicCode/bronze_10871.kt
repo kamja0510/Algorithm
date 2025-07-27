a778e58fa5a1f45bc716f7a0b0ab6ea9db162a44
kamja0510 <kamja990510@gmail.com>
2025-07-27 22:09:00 +0900

    [INIT] #1 baekjoon_10871

---
+++ src/main/java/baekjoon/barkingdog/basicCode/bronze_10871.kt
@@ -0,0 +1,21 @@
+package baekjoon.barkingdog.basicCode
+
+import java.io.*
+import java.util.*
+
+fun main(){
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    var st = StringTokenizer(br.readLine())
+
+    val n = st.nextToken().toInt()
+    val x = st.nextToken().toInt()
+
+    st = StringTokenizer(br.readLine())
+    repeat(n){
+        val number = st.nextToken().toInt()
+        if(number < x) bw.write("$number ")
+    }
+
+    bw.flush()
+}
