4149c1a5163cc0c348491eb3fdbd260b4a125858
kamja0510 <kamja990510@gmail.com>
2025-08-01 05:08:26 +0900

    [REFACTOR] #16 백준 2480 풀이 리팩토링

--- src/main/java/baekjoon/barkingdog/basicCode/Bronze2480.kt
+++ src/main/java/baekjoon/barkingdog/basicCode/Bronze2480.kt
@@ -9,19 +9,17 @@ fun main(){
     var max = -1
     while(st.hasMoreTokens()){
         val dice = st.nextToken().toInt()
-        if(map.containsKey(dice)){
-            map[dice] = map.getValue(dice) + 1
-        }else{
-            map[dice] = 1
-        }
+        map[dice] = map.getOrDefault(dice, 0) + 1
         if(dice > max) max = dice
     }
 
     val result = when{
         map.keys.size == 3 -> max * 100
-        map.keys.size == 2 -> 1_000 + map.filter { it.value == 2 }.keys.sum() * 100
-        map.keys.size == 1 -> 10_000 + map.keys.sum() * 1_000
+        map.keys.size == 2 -> 1_000 + map.entries.first { it.value == 2 }.key * 100
+        map.keys.size == 1 -> 10_000 + map.keys.first() * 1_000
         else -> 0
     }
     println(result)
 }
+
+// 14372KB, 100ms
---
+++ src/main/java/baekjoon/barkingdog/basicCode/Bronze2480V2.kt
@@ -0,0 +1,24 @@
+package baekjoon.barkingdog.basicCode
+
+import java.util.*
+
+fun main() {
+    val st = StringTokenizer(readln())
+    val count = IntArray(7) // 주사위는 1-6
+
+    while(st.hasMoreTokens()) {
+        count[st.nextToken().toInt()]++
+    }
+
+    val uniqueCount = count.count { it > 0 }
+    val result = when (uniqueCount) {
+        3 -> count.indexOfLast { it > 0 } * 100
+        2 -> 1_000 + count.indexOfFirst { it == 2 } * 100
+        1 -> 10_000 + count.indexOfFirst { it > 0 } * 1_000
+        else -> 0
+    }
+
+    println(result)
+}
+
+// 12428KB, 72ms
