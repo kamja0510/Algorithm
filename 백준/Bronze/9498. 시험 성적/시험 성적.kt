7a6d9790431eabe9eb4b038152fabbf138fc887e
kamja0510 <89302303+kamja0510@users.noreply.github.com>
2025-08-01 00:34:04 +0900

    [Bronze V] Title: 시험 성적, Time: 76 ms, Memory: 12392 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val score = readln().toInt()
+    when(score){
+        in 90..100 -> println("A")
+        in 80..89 -> println("B")
+        in 70..79 -> println("C")
+        in 60..69 -> println("D")
+        else -> println("F")
+    }
+}
