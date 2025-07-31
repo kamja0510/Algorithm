da2f9184573b24750d9b45226dcfe552ab2e2b3d
kamja0510 <kamja990510@gmail.com>
2025-07-31 23:58:50 +0900

    [SOLVE] #11 백준 10869 첫번째 풀이

---
+++ src/main/java/baekjoon/barkingdog/basicCode/Bronze10869.kt
@@ -0,0 +1,20 @@
+package baekjoon.barkingdog.basicCode
+
+import java.util.*
+
+fun main(){
+    val br = System.`in`.bufferedReader()
+    val bw = System.`out`.bufferedWriter()
+    val st = StringTokenizer(br.readLine())
+
+    val a = st.nextToken().toInt()
+    val b = st.nextToken().toInt()
+
+    bw.write("${a+b}\n")
+    bw.write("${a-b}\n")
+    bw.write("${a*b}\n")
+    bw.write("${a/b}\n")
+    bw.write("${a%b}\n")
+    bw.flush()
+    bw.close()
+}
