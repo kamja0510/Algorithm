04abd9bc7a1455f1a8b446d28ab6abbbeb4a5e9f
kamja0510 <89302303+kamja0510@users.noreply.github.com>
2025-08-01 05:07:32 +0900

    [Bronze IV] Title: 주사위 세개, Time: 72 ms, Memory: 12428 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.kt
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.kt
@@ -1,21 +1,20 @@
 import java.util.*
 
-fun main(){
+fun main() {
     val st = StringTokenizer(readln())
+    val count = IntArray(7) // 주사위는 1-6
 
-    val map = mutableMapOf<Int, Int>()
-    var max = -1
-    while(st.hasMoreTokens()){
-        val dice = st.nextToken().toInt()
-        map[dice] = map.getOrDefault(dice, 0) + 1
-        if(dice > max) max = dice
+    while(st.hasMoreTokens()) {
+        count[st.nextToken().toInt()]++
     }
 
-    val result = when{
-        map.keys.size == 3 -> max * 100
-        map.keys.size == 2 -> 1_000 + map.entries.first { it.value == 2 }.key * 100
-        map.keys.size == 1 -> 10_000 + map.keys.first() * 1_000
+    val uniqueCount = count.count { it > 0 }
+    val result = when (uniqueCount) {
+        3 -> count.indexOfLast { it > 0 } * 100
+        2 -> 1_000 + count.indexOfFirst { it == 2 } * 100
+        1 -> 10_000 + count.indexOfFirst { it > 0 } * 1_000
         else -> 0
     }
+
     println(result)
 }
