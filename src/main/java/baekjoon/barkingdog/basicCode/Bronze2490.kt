83e7077553fa48d7a34cc2e75610e04d2d2e60fe
kamja0510 <kamja990510@gmail.com>
2025-08-01 05:49:22 +0900

    [SOLVE] #17 백준 2490 풀이

---
+++ src/main/java/baekjoon/barkingdog/basicCode/Bronze2490.kt
@@ -0,0 +1,29 @@
+package baekjoon.barkingdog.basicCode
+
+import java.util.*
+
+fun main(){
+    print(
+        buildString {
+            repeat(3){
+                val st = StringTokenizer(readln())
+                var count = 0
+                while(st.hasMoreTokens()){
+                    val stomach = st.nextToken()
+                    if(stomach == "0") count++
+                }
+                appendLine(
+                    when(count){
+                        0 -> "E"
+                        1 -> "A"
+                        2 -> "B"
+                        3 -> "C"
+                        4 -> "D"
+                        else -> ""
+                    }
+                )
+            }
+        }
+    )
+}
+// 12340KB, 76ms
