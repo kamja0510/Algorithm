10586f19683d4b3bc0451f5e3df823f5df55af24
kamja0510 <89302303+kamja0510@users.noreply.github.com>
2025-08-02 22:57:52 +0900

    [Bronze II] Title: 대표값2, Time: 80 ms, Memory: 12376 KB -BaekjoonHub

--- 백준/Bronze/2587. 대표값2/대표값2.kt
+++ 백준/Bronze/2587. 대표값2/대표값2.kt
@@ -1,7 +1,29 @@
 package baekjoon.barkingdog.basicCode
 
 fun main() {
-    val numbers = List(5){ readln().toInt() }.sorted()
-    println(numbers.sum()/5)
-    println(numbers[2])
+    val n = 5
+    val num = Array(n) { 0 }
+    var sum = 0
+    repeat(n) {
+        num[it] = readln().toInt()
+        sum += num[it]
+    }
+    insertionSort(num)
+    print("${sum / n}\n${num[2]}")
+}
+
+fun insertionSort(list: Array<Int>) {
+    for (i in 1 until list.size) {
+        val key = list[i]
+        var j = i - 1
+
+        // 왼쪽에 있는 값들과 비교하면서 자리를 찾는다
+        while (j >= 0 && list[j] > key) {
+            list[j + 1] = list[j]
+            j--
+        }
+
+        // key 값을 올바른 위치에 삽입
+        list[j + 1] = key
+    }
 }
