addfdf1613066f4470c7b851da20a8e3b246e07b
kamja0510 <89302303+kamja0510@users.noreply.github.com>
2025-08-04 20:35:52 +0900

    [Bronze II] Title: 카드 역배치, Time: 96 ms, Memory: 12836 KB -BaekjoonHub

---
+++ 백준/Bronze/10804. 카드 역배치/카드 역배치.kt
@@ -0,0 +1,26 @@
+package baekjoon.barkingdog.basicCode
+
+import java.util.*
+
+fun main(){
+    val numbers = IntArray(21){ index -> index }
+    for(i in 0..9){
+        val st = StringTokenizer(readln())
+        var start = st.nextToken().toInt()
+        var end = st.nextToken().toInt()
+        while(start < end){
+            val temp = numbers[start]
+            numbers[start] = numbers[end]
+            numbers[end] = temp
+            start++
+            end--
+        }
+    }
+
+    val bw = System.`out`.bufferedWriter()
+
+    for(i in 1..20){
+        bw.write("${numbers[i]} ")
+    }
+    bw.close()
+}
