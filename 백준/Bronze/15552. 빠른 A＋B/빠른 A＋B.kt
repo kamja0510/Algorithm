ad4f15ae7816813fe92ef2d86217656ab1972d6d
kamja0510 <89302303+kamja0510@users.noreply.github.com>
2025-08-05 17:04:00 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 796 ms, Memory: 298460 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.kt
@@ -0,0 +1,16 @@
+package baekjoon.barkingdog.basicCode
+
+import java.util.StringTokenizer
+
+fun main() {
+    System.`in`.bufferedReader().use { br ->
+        System.`out`.bufferedWriter().use { bw ->
+            val t = br.readLine().toInt()
+            repeat(t) {
+                val st = StringTokenizer(br.readLine())
+                bw.write("${st.nextToken().toInt()+st.nextToken().toInt()}\n")
+            }
+            bw.flush()
+        }
+    }
+}
