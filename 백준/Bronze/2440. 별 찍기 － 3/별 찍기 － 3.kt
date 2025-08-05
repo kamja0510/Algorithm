af66995453ec32ebcb53a430a30c13cb2807daf9
kamja0510 <89302303+kamja0510@users.noreply.github.com>
2025-08-05 18:18:30 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 76 ms, Memory: 12444 KB -BaekjoonHub

--- 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.kt
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.kt
@@ -2,13 +2,13 @@ package baekjoon.barkingdog.basicCode
 
 fun main(){
     val n = readln().toInt()
-    
+
     val result = buildString{
         for(i in n downTo 1){
-            repeat(i){append("*")}
+            repeat(i){append('*')}
             appendLine()
         }
     }
-    
+
     print(result)
 }
