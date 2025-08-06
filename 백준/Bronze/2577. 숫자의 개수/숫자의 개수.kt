ff747c1f3802f7520b63c70591a0892e6fc2c209
kamja0510 <89302303+kamja0510@users.noreply.github.com>
2025-08-06 09:49:24 +0900

    [Bronze II] Title: 숫자의 개수, Time: 72 ms, Memory: 12372 KB -BaekjoonHub

---
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.kt
@@ -0,0 +1,22 @@
+package baekjoon.barkingdog.array
+
+fun main(){
+    var multipleNumber = 1
+    val numbers = IntArray(10)
+    repeat(3){
+        multipleNumber *= readln().toInt()
+    }
+    
+    multipleNumber.toString().forEach{
+        numbers[it.code - '0'.code]++
+    }
+    
+    val result = buildString {
+        numbers.forEach {
+            append(it)
+            appendLine()
+        }
+    }
+    
+    print(result)
+}
