0c204bfbc9748027d19555113eaf1540fb9d8a0f
kamja0510 <kamja990510@gmail.com>
2025-08-06 23:00:38 +0900

    [REFACTOR] #65 백준 11328 풀이 리팩토링

--- src/main/java/baekjoon/barkingdog/array/Bronze11328.kt
+++ src/main/java/baekjoon/barkingdog/array/Bronze11328.kt
@@ -4,7 +4,7 @@ import java.util.*
 
 fun main() {
     System.`in`.bufferedReader().use { br ->
-        val t = readln().toInt()
+        val t = br.readLine().toInt()
         val result = buildString {
             repeat(t) {
                 val charArray = ShortArray(26)
@@ -12,13 +12,13 @@ fun main() {
                 val oldString = st.nextToken()
                 val newString = st.nextToken()
                 oldString.forEach {
-                    charArray[it.code-'a'.code]++
+                    charArray[it -'a']++
                 }
                 newString.forEach {
-                    charArray[it.code-'a'.code]--
+                    charArray[it-'a']--
                 }
-                val filteredSize = charArray.filterNot { it.toInt() == 0 }.size
-                if(filteredSize == 0) append("Possible") else append("Impossible")
+                val isPossible = charArray.none { it != 0.toShort() }
+                if(isPossible) append("Possible") else append("Impossible")
                 appendLine()
             }
         }
