c62665860f661e3734e7d7cd2a0683baff90a8ec
kamja0510 <89302303+kamja0510@users.noreply.github.com>
2025-08-06 15:48:10 +0900

    [Bronze II] Title: 방 배정, Time: 116 ms, Memory: 15172 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/13300. 방 배정/방 배정.kt
+++ 백준/Bronze/13300. 방 배정/방 배정.kt
@@ -8,23 +8,23 @@ fun main(){
         var st = StringTokenizer(readln())
         val n = st.nextToken().toInt()
         val capacity = st.nextToken().toFloat()
-        
+
         val peopleTable = Array(2){ IntArray(6) }
         var numberOfRoom = 0
-        
+
         repeat(n){
             st = StringTokenizer(readln())
             val sex = st.nextToken().toInt()
             val grade = st.nextToken().toInt()
-            peopleTable[sex][grade]++
+            peopleTable[sex][grade-1]++
         }
-        
+
         peopleTable.forEach {
-            it.forEach { numberOfPerson -> 
+            it.forEach { numberOfPerson ->
                 numberOfRoom += ceil(numberOfPerson / capacity).toInt()
             }
         }
-        
+
         print(numberOfRoom)
     }
 }
