96f0021ed6c1c18aa366801b9c26c30f579752cf
kamja0510 <89302303+kamja0510@users.noreply.github.com>
2025-08-10 00:42:24 +0900

    [Silver II] Title: 에디터, Time: 376 ms, Memory: 58616 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.kt
+++ 백준/Silver/1406. 에디터/에디터.kt
@@ -1,50 +1,47 @@
 package baekjoon.barkingdog.linkedList
 
 import java.util.LinkedList
-import java.util.StringTokenizer
 
 fun main(){
     System.`in`.bufferedReader().use{ br ->
-        System.`out`.bufferedWriter().use { bw ->
-            val linkedList = LinkedList<Char>()
-            val iterator = linkedList.listIterator()
-            while (true) {
-                val alphabet = br.read().toChar()
-                if (alphabet == '\n') break
-                iterator.add(alphabet)
-            }
+        val linkedList = LinkedList<Char>()
+        val iterator = linkedList.listIterator()
+        while (true) {
+            val alphabet = br.read().toChar()
+            if (alphabet == '\n') break
+            iterator.add(alphabet)
+        }
 
-            val m = br.readLine().toInt()
+        val m = br.readLine().toInt()
 
-            repeat(m) {
-                val command = br.readLine()
+        repeat(m) {
+            val command = br.readLine()
 
-                when (command[0]) {
-                    'L' -> {
-                        if (iterator.hasPrevious()) iterator.previous()
-                    }
+            when (command[0]) {
+                'L' -> {
+                    if (iterator.hasPrevious()) iterator.previous()
+                }
 
-                    'D' -> {
-                        if (iterator.hasNext()) iterator.next()
-                    }
+                'D' -> {
+                    if (iterator.hasNext()) iterator.next()
+                }
 
-                    'B' -> {
-                        if (iterator.hasPrevious()) {
-                            iterator.previous()
-                            iterator.remove()
-                        }
-                    }
-
-                    'P' -> {
-                        val alphabet = command[2]
-                        iterator.add(alphabet)
+                'B' -> {
+                    if (iterator.hasPrevious()) {
+                        iterator.previous()
+                        iterator.remove()
                     }
                 }
+
+                'P' -> {
+                    val alphabet = command[2]
+                    iterator.add(alphabet)
+                }
             }
-            val result = buildString {
-                linkedList.forEach { append(it) }
-            }
-            bw.write(result)
         }
+        val result = buildString {
+            linkedList.forEach { append(it) }
+        }
+        println(result)
     }
 }
