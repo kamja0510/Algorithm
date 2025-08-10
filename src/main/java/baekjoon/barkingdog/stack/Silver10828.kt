c699c4a4a879c153d3777c852209545bd8eca972
kamja0510 <kamja990510@gmail.com>
2025-08-10 16:19:49 +0900

    [SOLVE] #81 백준 10828 풀이

---
+++ src/main/java/baekjoon/barkingdog/stack/Silver10828.kt
@@ -0,0 +1,33 @@
+package baekjoon.barkingdog.stack
+
+import java.util.*
+
+fun main() {
+    val numberOfCommands = readln().toInt()
+
+    val stack = Stack<Int>()
+
+    buildString {
+        repeat(numberOfCommands){
+            val command = readln().split(" ")
+
+            when(command[0]){
+                "push" ->{
+                    stack.push(command[1].toInt())
+                }
+                "pop" -> {
+                    if(stack.isEmpty()) appendLine(-1) else appendLine(stack.pop())
+                }
+                "size" -> {
+                    appendLine(stack.size)
+                }
+                "empty" -> {
+                    if(stack.isEmpty()) appendLine(1) else appendLine(0)
+                }
+                "top" -> {
+                    if(stack.isEmpty()) appendLine(-1) else appendLine(stack.peek())
+                }
+            }
+        }
+    }.run { print(this) }
+}
