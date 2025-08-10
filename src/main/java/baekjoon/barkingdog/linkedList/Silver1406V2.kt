41a0c544a24f508fb122cb2b2435ab7da07ae2c4
kamja0510 <kamja990510@gmail.com>
2025-08-10 10:00:29 +0900

    [SOLVE] #75 백준 1406 풀이

---
+++ src/main/java/baekjoon/barkingdog/linkedList/Silver1406V2.kt
@@ -0,0 +1,24 @@
+package baekjoon.barkingdog.linkedList
+
+fun main() = System.`in`.bufferedReader().use { br ->
+    val left = ArrayDeque<Char>() // 커서 왼쪽
+    val right = ArrayDeque<Char>() // 커서 오른쪽
+
+    br.readLine().forEach { left.addLast(it) }
+
+    repeat(br.readLine().toInt()) {
+        val cmd = br.readLine()
+        when (cmd[0]) {
+            'L' -> if (left.isNotEmpty()) right.addFirst(left.removeLast())
+            'D' -> if (right.isNotEmpty()) left.addLast(right.removeFirst())
+            'B' -> if (left.isNotEmpty()) left.removeLast()
+            'P' -> left.addLast(cmd[2])
+        }
+    }
+
+    val result = buildString {
+        left.forEach { append(it) }
+        right.forEach { append(it) }
+    }
+    println(result)
+}
