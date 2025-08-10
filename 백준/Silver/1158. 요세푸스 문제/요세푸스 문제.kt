10dcdd905c26f2619949e23558094063dec313bb
kamja0510 <89302303+kamja0510@users.noreply.github.com>
2025-08-10 15:16:38 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 144 ms, Memory: 22156 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.kt
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.kt
@@ -1,53 +1,17 @@
 package baekjoon.barkingdog.linkedList
 
 fun main() {
-    val (n,k) = readln().split(' ').map(String::toInt)
-    val circularLinkedList = CircularLinkedList<Int>()
-    (1..n).forEach{ circularLinkedList.add(it)}
-    buildString {
-        append('<')
-        while(circularLinkedList.getSize() > 0){
-            append(circularLinkedList.removeNext(k))
-            if(circularLinkedList.getSize() != 0)append(", ")
-        }
-        append('>')
-    }.run(::println)
-}
+    val (n, k) = readln().split(" ").map { it.toInt() }
 
-class CircularLinkedList<T> {
-    data class Node<T>(
-        val value: T,
-        var next: Node<T>? = null,
-    )
+    val list = MutableList(n) { it + 1 }
 
-    private var tail: Node<T>? = null
-    private var cur: Node<T>? = null
-    private var size = 0
+    val answer = mutableListOf<Int>()
 
-    fun add(value: T){
-        val newNode = Node(value)
-        if(tail == null) {
-            tail = newNode
-            newNode.next = tail
-        }
-        else{
-            newNode.next = tail!!.next
-            tail!!.next = newNode
-            tail = newNode
-        }
-        cur = tail
-        size++
+    var index = 0
+    while (list.isNotEmpty()) {
+        index = (index + k - 1) % list.size
+        answer.add(list.removeAt(index))
     }
 
-    fun removeNext(step: Int = 1): T{
-        repeat(step-1){
-            cur = cur!!.next
-        }
-        val after: Node<T>? = cur!!.next
-        cur!!.next = after!!.next
-        size--
-        return after.value
-    }
-
-    fun getSize(): Int = size
+    println("<${answer.joinToString()}>")
 }
