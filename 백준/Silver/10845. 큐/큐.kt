7b019006072712864278cd48bf403762cd5576f7
kamja0510 <89302303+kamja0510@users.noreply.github.com>
2025-09-01 23:48:06 +0900

    [Silver IV] Title: 큐, Time: 272 ms, Memory: 29124 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.kt
+++ 백준/Silver/10845. 큐/큐.kt
@@ -3,15 +3,18 @@ package baekjoon.barkingdog.queue
 fun main(){
     val numberOfCommands = readln().toInt()
     val queue = ArrayDeque<Int>()
-    repeat(numberOfCommands){
-        val command = readln().split(" ")
-        when(command[0]){
-            "push" -> queue.addLast(command[1].toInt())
-            "pop" -> if(queue.isNotEmpty()) println(queue.removeFirst()) else println("-1")
-            "size" -> println(queue.size)
-            "empty" -> println(if(queue.isEmpty()) 1 else 0)
-            "front" -> println(if(queue.isNotEmpty()) queue.first() else "-1")
-            "back" -> println(if(queue.isNotEmpty()) queue.last() else "-1")
+    val result = buildString {
+        repeat(numberOfCommands){
+            val command = readln().split(" ")
+            when(command[0]){
+                "push" -> queue.addLast(command[1].toInt())
+                "pop" -> if(queue.isNotEmpty()) appendLine(queue.removeFirst()) else appendLine(-1)
+                "size" -> appendLine(queue.size)
+                "empty" -> appendLine(if(queue.isEmpty()) 1 else 0)
+                "front" -> appendLine(if(queue.isNotEmpty()) queue.first() else -1)
+                "back" -> appendLine(if(queue.isNotEmpty()) queue.last() else -1)
+            }
         }
     }
+    print(result)
 }
