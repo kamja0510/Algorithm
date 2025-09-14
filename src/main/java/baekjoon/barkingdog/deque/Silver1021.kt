2f628ccfc894d8688368a03b0159941b54206da1
kamja0510 <kamja990510@gmail.com>
2025-09-14 21:43:08 +0900

    [SOLVE] #104 백준 1021 풀이

---
+++ src/main/java/baekjoon/barkingdog/deque/Silver1021.kt
@@ -0,0 +1,22 @@
+package baekjoon.barkingdog.deque
+
+import kotlin.math.absoluteValue
+
+fun main(){
+    var (sizeOfQueue, numberOfExtracting) = readln().split(" ").map{ it.toInt() }
+    val orderOfExtracting = readln().split(" ").map{ it.toInt() }
+    var answer = 0
+    var offset = 0
+    orderOfExtracting.forEach{
+        val currentLocation =
+            if(((it + offset) % sizeOfQueue) <= 0) sizeOfQueue + ((it + offset) % sizeOfQueue )
+            else (it + offset) % sizeOfQueue
+        val amount = if(sizeOfQueue - currentLocation + 1> currentLocation - 1) -(currentLocation -1) else sizeOfQueue - currentLocation + 1
+        answer += amount.absoluteValue
+        offset = offset + amount - 1
+        sizeOfQueue --
+    }
+    println(answer)
+}
+
+// 실패한 코드
