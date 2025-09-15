5b034b7fe95e97cf24ddd531a92d6850ef3c60da
kamja0510 <89302303+kamja0510@users.noreply.github.com>
2025-09-15 17:55:28 +0900

    [Gold V] Title: 괄호의 값, Time: 120 ms, Memory: 18728 KB -BaekjoonHub

--- 백준/Gold/2504. 괄호의 값/괄호의 값.kt
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.kt
@@ -1,29 +1,26 @@
 package baekjoon.barkingdog.stack2
 
-import kotlin.math.pow
-
 fun main(){
     val brackets = readln()
     var answer = 0
     val stack = ArrayDeque<Char>(0)
-    var parenthesesCount = 0
-    var bracketsCount = 0
+    var mul = 1
     var isCorrect = true
     for(i in brackets.indices){
         when(brackets[i]){
             '(' -> {
                 stack.addLast('(')
-                parenthesesCount++
+                mul *= 2
             }
             '[' -> {
                 stack.addLast('[')
-                bracketsCount++
+                mul *= 3
             }
             ')' -> {
                 if(stack.lastOrNull() == '(') {
                     stack.removeLast()
-                    parenthesesCount--
-                    if(brackets[i-1] == '(') answer += 2 *  2.0.pow(parenthesesCount).toInt() * 3.0.pow(bracketsCount).toInt()
+                    mul /= 2
+                    if(brackets[i-1] == '(') answer += 2 * mul
                 }else{
                     isCorrect = false
                     break
@@ -32,8 +29,8 @@ fun main(){
             ']' -> {
                 if(stack.lastOrNull() == '[') {
                     stack.removeLast()
-                    bracketsCount--
-                    if(brackets[i-1] == '[') answer += 3 *  2.0.pow(parenthesesCount).toInt() * 3.0.pow(bracketsCount).toInt()
+                    mul /= 3
+                    if(brackets[i-1] == '[') answer += 3 *  mul
                 }else{
                     isCorrect = false
                     break
