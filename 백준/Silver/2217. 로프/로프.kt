d825ee8f205c5ca5150b2c7e06bf2045769d46dd
kamja0510 <89302303+kamja0510@users.noreply.github.com>
2025-11-08 03:46:33 +0900

    [Silver IV] Title: 로프, Time: 404 ms, Memory: 27988 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.kt
@@ -0,0 +1,13 @@
+package baekjoon.barkingdog.greedy
+
+fun main(){
+    val n = readln().toInt()
+    val ropes = List(n){readln().toInt()}.sorted()
+    
+    var maxWeight = 0
+    for(i in ropes.indices){
+        maxWeight = maxOf(ropes[i] * (n-i), maxWeight)
+    }
+    
+    println(maxWeight)
+}
