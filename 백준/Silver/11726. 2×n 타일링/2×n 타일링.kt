77b0b5f46f28d4a700cabe260ac8ffdf32e36e34
kamja0510 <89302303+kamja0510@users.noreply.github.com>
2025-11-08 06:45:49 +0900

    [Silver III] Title: 2×n 타일링, Time: 76 ms, Memory: 12408 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.kt
@@ -0,0 +1,26 @@
+package baekjoon.barkingdog.dp
+
+fun main(){
+    val n = readln().toInt()
+
+    when(n){
+        1 -> {
+            println(1)
+            return
+        }
+        2 -> {
+            println(2)
+            return
+        }
+    }
+    
+    val dp = IntArray(n+1){0}
+    dp[1] = 1
+    dp[2] = 2
+    for(i in 3..n){
+        dp[i] = dp[i-1] + dp[i-2]
+        dp[i] %= 10007
+    }
+
+    println(dp[n])
+}
